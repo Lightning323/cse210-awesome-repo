a06b2df13646cb31ab0be11d6bdc53ea60df1704
Lightning323 <sam.p.w524@gmail.com>
2026-06-15 13:28:12 -0600

    commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using Learning05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("red", 20));
+        shapes.Add(new Rectangle("blue", 20, 20));
+        shapes.Add(new Square("green", 20));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($" A {shape.GetColor()} {shape.GetType().Name} has an area of {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,8 @@
+namespace Learning05;
+
+public class Square : Rectangle
+{
+    public Square(string color, double sideLength) : base(color, sideLength, sideLength)
+    {
+    }
+}
